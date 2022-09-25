6193aaca0b62c44be7bab77f7b2396c6e5f11c5a
George W. Toumbas <gtoumbas@courselab04.cs.princeton.edu>
2022-09-25 17:58:29 -0400

    test

--- tests.py
+++ tests.py
@@ -79,4 +79,4 @@ def test_with_file(arg_file, correct_file, our_file):
         
 
 test_with_file('reg_test_args.txt', CORRECT_REG, OUR_REG)
-# test_with_file('regdetails_test_args.txt', OUR_REGDETAILS, OUR_REGDETAILS)
+test_with_file('regdetails_test_args.txt', CORRECT_REGDETAILS, OUR_REGDETAILS)
