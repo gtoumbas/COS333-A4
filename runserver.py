e41b6d46812bebc4eb9464625f1321decc78b602
Shanzay Waseem <shanzayijaz@gmail.com>
2022-10-27 15:34:34 +0500

    completed cookies

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 import argparse
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, make_response
 from reg_db import RegDB
 # Flask home route
 # test json data of classes with dept, number, area, title
@@ -49,10 +49,40 @@ def home():
         results = db.search(params)
         db.close()
 
+        set_currentinputs = None
+        inputs = {"dept": dept or '', "num":num or '', \
+            "area":area or '', "title":title or ''}
+        if any(inputs):
+            set_currentinputs = inputs
+        else:
+            set_currentinputs = get_prevcookies()
+        page = render_template('reg_search.html', courses=results, **set_currentinputs)
+        response = make_response(page)
+        set_newcookies(response, inputs)
 
-        return render_template('reg_search.html', courses=results)
-    
-    # return render_template('reg_search.html')
+        return response
+
+
+def get_prevcookies():
+    dept = request.cookies.get("dept") or None
+    num = request.cookies.get("num") or None
+    area = request.cookies.get("area") or None
+    title = request.cookies.get("title") or None
+    inputs = {"dept": dept, "num":num,"area":area, "title":title}
+    return inputs
+
+
+def set_newcookies(response, inputs):
+    if inputs["dept"] is not None:
+        response.set_cookie("dept", inputs["dept"])
+    if inputs["num"] is not None:
+        response.set_cookie("num", inputs["num"])
+    if inputs["area"] is not None:
+        response.set_cookie("area", inputs["area"])
+    if inputs["title"] is not None:
+        response.set_cookie("title", inputs["title"])
+
+
 
 @app.route('/regdetails', methods=['GET'])
 def details():
@@ -94,7 +124,7 @@ def details():
 
         course_results["dept_num"] = set(course_results["dept_num"])
         course_results["profs"] = set(course_results["profs"])
-        
+
         return render_template('reg_details.html', course=course_results)
 
 
